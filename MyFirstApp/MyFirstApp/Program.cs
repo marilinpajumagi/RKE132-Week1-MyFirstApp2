7dd180e2ab7addf25ceeadc3d7ca42df30c3e02a
marilinpajumagi <154065653+marilinpajumagi@users.noreply.github.com>
2023-12-17 15:40:30 +0200

    Add files via upload

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisetada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string - sõne
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !");
